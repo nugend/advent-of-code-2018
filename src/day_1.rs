f46451a9c53dcbfb38a5c2cb02bac8e5ed0ae173
Dan Nugent <nugend@gmail.com>
2018-12-01 14:36:51 -0500

    Fixed up day 1 attempt after manually testing in playground

--- src/day_1.rs
+++ src/day_1.rs
@@ -2,8 +2,8 @@ use aoc_runner_derive::{aoc_generator, aoc};
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
-    input.lines().map(|l| i32::from(l)).collect()
+    input.lines().map(|l| l.trim().parse::<i32>().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
-pub fn solve(input:&[i32]) -> i64 { input.sum() }
+pub fn solve(input:&[i32]) -> i64 { input.iter().map(|&x|x as i64).sum() }
